6b7405423600cc3921a18b2cae8a9c121e78aa2d
Orbi1025 <john941025@gmail.com>
2025-11-01 15:34:02 -0400

    i

--- _next/static/chunks/pages/index-74109990826b0d7a.js
+++ _next/static/chunks/pages/index-74109990826b0d7a.js
@@ -771,7 +771,7 @@
                         className: "flex gap-4",
                         children: [
                           (0, R.jsx)("a", {
-                            href: "https://t.me/Goro_MattFurie",
+                            href: "https://t.me/MattFurie_Goro",
                             target: "_blank",
                             rel: "noopener noreferrer",
                             className:
@@ -788,7 +788,7 @@
                             }),
                           }),
                           (0, R.jsx)("a", {
-                            href: "https://x.com/Goro_MattFurie",
+                            href: "https://x.com/MattFurie_Goro",
                             target: "_blank",
                             rel: "noopener noreferrer",
                             className:
